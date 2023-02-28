bbbf535b4321607d16231c91d11d64cc681aa1fd
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-02-28 20:43:04 +0300

    rename test__repr__ and add docstring

--- tests/test_Stack.py
+++ tests/test_Stack.py
@@ -4,11 +4,13 @@ from datastruct.Stack import Stack
 
 class TestStack(unittest.TestCase):
 
-    def test__repr__(self):
+    def test_get_stack(self):
+        """Ожидается инициализация стека"""
         self.assertEqual(Stack.__repr__(Stack()), "Stack('top'=None)")
         self.assertEqual(Stack.__repr__(Stack(5)), "Stack('top'=5)")
 
     def test_push(self):
+        """Ожидается добавление элемента в конец"""
         s = Stack()
         s.push('data1')
         s.push('data2')
@@ -17,6 +19,7 @@ class TestStack(unittest.TestCase):
         self.assertEqual(s.top.next_node.next_node, None)
 
     def test_pop(self):
+        """Ожидается удаление последнего добавленного элемента"""
         s = Stack()
         s.push('data1')
         s.push('data2')
