100ac3e1d355442223503bd2374e1237942767c1
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-03-08 19:48:38 +0300

    Add check_data(), to_list, get_data_by_id and tests

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -37,6 +37,12 @@ class TestLinkedTest(unittest.TestCase):
         self.assertEqual(self.ll.head.next_node, None)
         self.assertEqual(self.ll.tail.next_node, None)
 
+    def test_insert_incorrect_element(self):
+        """Ожидается обработка исключения TypeError при добавлении некорректного элемента"""
+        result = 'Данные не являются словарем или в словаре нет id.'
+        self.assertEqual(self.ll.insert_beginning([1, 2, 3]), print(result))
+        self.assertEqual(self.ll.insert_at_end({'one': 1}), print(result))
+
     def test_check_linked_list(self):
         """Проверяет правильность хранения ссылок"""
         self.assertEqual(self.ll2.head.data, {'id': 0})
@@ -49,3 +55,13 @@ class TestLinkedTest(unittest.TestCase):
     def test_print_ll(self):
         """Ожидаются все данные односвязного списка"""
         self.assertEqual(self.ll2.print_ll_(), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+    def test_to_list(self):
+        """Ожидается list с данными односвязного списка"""
+        self.assertEqual(self.ll.to_list(), None)
+        self.assertIsInstance(self.ll2.to_list(), list)
+
+    def test_get_data_by_id(self):
+        """Ожидается получение первого найденного словаря по переданному значению"""
+        self.assertEqual(self.ll2.get_data_by_id(3), {'id': 3})
+        self.assertEqual(self.ll2.get_data_by_id(4), None)
